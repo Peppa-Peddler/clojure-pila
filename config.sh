b997a40ddb46e638f5006564b9c97ce1e4312a3d
peppa-peddler <peppasama@gmail.com>
2016-12-29 23:04:01 -0500

    fixing java install

--- config.sh
+++ config.sh
@@ -10,12 +10,16 @@ apt-get -y install unzip
 apt-get -y install git
 
 # Install Java 8
-sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get install -y software-properties-common python-software-properties
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-sudo apt-get install -y oracle-java{7,8}-installer maven
-sudo update-java-alternatives -s java-8-oracle
+sudo apt-get install oracle-java8-installer
+echo "Setting environment variables for Java 8.."
+sudo apt-get install -y oracle-java8-set-default
 
 # Install leiningen
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 chmod +x lein
+echo "Setting lein.."
 sudo mv lein /usr/local/bin
